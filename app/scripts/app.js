5063681f94045d4d688e6f21525ae481f3a936ee
I3rendan <brendan@brendanenglish.com>
2016-03-16 09:14:21 -0400

    master

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('quantumThinkingApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
